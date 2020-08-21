1d0ddbce5894538c5d7afa7940ce7ea8e59cffa6
Kirill Shvedov <github.shvedov@gmail.com>
2020-08-21 21:10:55 +0300

    Update Find_Dublicates.py

--- Find_Dublicates.py
+++ Find_Dublicates.py
@@ -7,12 +7,10 @@
 
 array = [1,2,2,4,4,5,5,6,7,8,9,9,10]
 def Find_Dublicates(arr):
-	#x = []
 	for i in range(len(arr)):
 		for j in range(len(arr)):
 			if i != j:
 				if (arr[i] == arr[j]):
-					#x.append(arr[i])
 					return True
 	return False
 	
